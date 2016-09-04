943164f5a251d86537f0bd1c82cb4e990cb0fd1e
Rohit Kaundal <kaundal.rohit@gmail.com>
2016-09-04 23:21:30 +0530

    API debudded

--- application/controllers/APIv1/Api.php
+++ application/controllers/APIv1/Api.php
@@ -4,6 +4,18 @@ require(APPPATH.'/libraries/REST_Controller.php');
 
 class Api extends REST_Controller{
 	
+	function __construct()
+	{
+		parent::__construct();
+		header('Access-Control-Allow-Origin: *');
+		header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+		header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+		$method = $_SERVER['REQUEST_METHOD'];
+		if($method == "OPTIONS") {
+			die();
+		}
+	}
+	
 	/**
 	* Users api
 	*/
