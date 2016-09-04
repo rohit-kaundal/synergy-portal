859e5246616e97211d347245b3744d4f292ca52f
Rohit Kaundal <kaundal.rohit@gmail.com>
2016-09-04 20:31:36 +0530

    Created single Api file containing endpoints of CRUD of users

---
+++ application/controllers/APIv1/Api.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require(APPPATH.'/libraries/REST_Controller.php');
+
+class Api extends REST_Controller{
+	
+	/**
+	* Users api
+	*/
+	function users_get()
+	{
+		$id = $this->get('id');
+		if($id){
+			$user = $this->userdetails_model->get_all_details_ofid($id);
+			if($user){
+				$this->response($user,200);	
+			}else{
+				$this->response(null,404);
+			}
+			
+		}else{
+			$users = $this->userdetails_model->get_all_details();
+			if($users){
+				$this->response($users,200);	
+			}else{
+				$this->response(null,404);
+			}
+			
+		}
+	}
+}
+
+?>
