d1c2569011b8ff7089cc2c860f60db6f957961e7
Rohit Kaundal <kaundal.rohit@gmail.com>
2016-09-05 22:10:20 +0530

    Angular and its libraries added

--- application/views/survey_builder.php
+++ application/views/survey_builder.php
@@ -7,7 +7,20 @@
 	
 	
 ?>
-<div class="panel panel-primary panel-shadow">
+<script type="text/javascript" src="<?= base_url('assets/js/angular/angular.js') ?>"></script>
+<script type="text/javascript">
+	angular.module('appSurvey',[])
+	.controller('myCtrl', ['$scope', function($scope){
+		$scope.btnClick = function(){
+			$scope.txtDesc = "Clicked !";
+		}
+		
+	}]);
+</script>
+
+<link rel="stylesheet" href="<?= base_url('assets/js/angular/angular-csp.css')?>">
+
+<div class="panel panel-primary panel-shadow" ng-app="appSurvey" ng-controller="myCtrl" ng-cloak>
 	<div class="panel-heading">
 		<div class="panel-title">
 			<h3><i class="entypo-pencil"></i>Add Questions to <span class="green"><?= $survey->title ?></span></h3>
@@ -19,8 +32,12 @@
 	
 				
 		<div class="tab-content">
-			<div class="row">
-				<div class="col"><p><?= $survey->description;?></p></div>
+			<div class="form-group">
+				<div class="input-group">
+					<p><?= $survey->description;?></p>
+					<p>{{txtDesc}}</p>
+				</div>
+				<button class="btn btn-success btn-sm btn-icon icon-left" ng-click="btnClick()"><i class="entypo-pencil"></i>Check for angular</button>
 			</div>
 			
 		</div>
