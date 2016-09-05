0dc90e4424f623ea3096acd152ae12f167eaddaf
Rohit Kaundal <kaundal.rohit@gmail.com>
2016-09-05 20:45:34 +0530

    under devel

--- application/views/survey_builder.php
+++ application/views/survey_builder.php
@@ -1,8 +1,29 @@
 <?php
 	$survey = $this->survey_model->get_survey($id);
+	
 	if(empty($survey)){
 		redirect(site_url('dashboard/edit_survey'));
 	}
 	
-	print_r($survey);	
+	
 ?>
+<div class="panel panel-primary panel-shadow">
+	<div class="panel-heading">
+		<div class="panel-title">
+			<h3><i class="entypo-pencil"></i>Add Questions to <span class="green"><?= $survey->title ?></span></h3>
+		</div>
+		
+	</div>
+
+	<div class="panel-body">
+	
+				
+		<div class="tab-content">
+			<div class="row">
+				<div class="col"><p><?= $survey->description;?></p></div>
+			</div>
+			
+		</div>
+	</div>
+</div>
+	
