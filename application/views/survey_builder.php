cddb892edea398ecc1661b1809d76290f5f607ea
Rohit Kaundal <kaundal.rohit@gmail.com>
2016-09-05 23:01:30 +0530

    surveyjs no use

--- application/views/survey_builder.php
+++ application/views/survey_builder.php
@@ -7,6 +7,7 @@
 	
 	
 ?>
+
 <script type="text/javascript" src="<?= base_url('assets/js/angular/angular.js') ?>"></script>
 <script type="text/javascript">
 	angular.module('appSurvey',[])
@@ -18,9 +19,9 @@
 	}]);
 </script>
 
-<link rel="stylesheet" href="<?= base_url('assets/js/angular/angular-csp.css')?>">
 
-<div class="panel panel-primary panel-shadow" ng-app="appSurvey" ng-controller="myCtrl" ng-cloak>
+
+<div class="panel panel-primary panel-shadow" ng-cloak ng-app="appSurvey" ng-controller="myCtrl">
 	<div class="panel-heading">
 		<div class="panel-title">
 			<h3><i class="entypo-pencil"></i>Add Questions to <span class="green"><?= $survey->title ?></span></h3>
@@ -34,8 +35,8 @@
 		<div class="tab-content">
 			<div class="form-group">
 				<div class="input-group">
-					<p><?= $survey->description;?></p>
-					<p>{{txtDesc}}</p>
+					<h3 ng-bind="txtDesc"></h3>
+										
 				</div>
 				<button class="btn btn-success btn-sm btn-icon icon-left" ng-click="btnClick()"><i class="entypo-pencil"></i>Check for angular</button>
 			</div>
