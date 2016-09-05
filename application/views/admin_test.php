cc9ab50b966f676a10add2ab25b013104c964214
Rohit Kaundal <kaundal.rohit@gmail.com>
2016-09-05 17:56:03 +0530

    Questions url added to edit survey view

--- application/views/admin_test.php
+++ application/views/admin_test.php
@@ -11,39 +11,16 @@
 		* 
 		*/
 		
-		print "<br/>====================Create test================<br/>";
-		for($id = 1; $id <= 5; $id++){
-			$this->vote_model->id = 0;
-			$this->vote_model->questionid = $id;
-			$this->vote_model->answerid = $id * 3;
-			$this->vote_model->respondantind = $id * 5;
-			$this->vote_model->surveyid = $id * $id;
-			$this->vote_model->save();
-		}
-		print_r($this->vote_model->get_votes());
+		print "<br/>====================Create test================<br/>";		
 		
 		print "<br/>====================Update test================<br/>";
-			$this->vote_model->id = 3;
-			$this->vote_model->questionid = 33;
-			$this->vote_model->answerid = 33;
-			$this->vote_model->respondantind = 33;
-			$this->vote_model->surveyid = 33;
-			$this->vote_model->save();
-			print_r($this->vote_model->get_votes());
-			
+		
 		print "<br/>====================Delete test================<br/>";
-			$this->vote_model->id = 4;
-			$this->vote_model->delete();
-			print_r($this->vote_model->get_votes());
 		
 		print "<br/>====================Read single test================<br/>";
-			print_r($this->vote_model->get_vote(2));
+		
 			
-		print "<br/>====================Read by respondant test================<br/>";
-			print_r($this->vote_model->get_votes_byrespondant(25));
-			
-		print "<br/>====================Read by awnser test================<br/>";
-			print_r($this->vote_model->get_votes_onanswers(3));
+	
 		
 		
 		
